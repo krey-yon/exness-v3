1c7eb8b3e76069df107ee99233d4f281287065aa
vikas <vikaskumar783588@gmail.com>
2025-09-04 21:47:31 +0530

    minor changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,10 @@
-import { prisma } from "@repo/database";
-
 export default async function IndexPage() {
   // const users = await prisma.user.findMany();
 
   return (
     <div>
       <h1>Hello World</h1>
+      <p>exness</p>
       {/* <pre>{JSON.stringify(users, null, 2)}</pre> */}
     </div>
   );
